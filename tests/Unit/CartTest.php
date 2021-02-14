b23aa1f407c7c0216d598a491b401ea3866e0c15
rami <rami.aouinti@gmail.com>
2021-02-15 00:29:53 +0100

    Unit Test

---
+++ tests/Unit/CartTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Tests\Unit;
+
+use App\Entity\CartItem;
+use App\Entity\Product;
+use App\Entity\User;
+use PHPUnit\Framework\TestCase;
+
+class CartTest extends TestCase
+{
+    private CartItem $cart;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->cart = new CartItem();
+    }
+
+    public function testGetQuantity(): void
+    {
+        $value = 5;
+        $this->cart->setQuantity($value);
+        $response = $this->cart;
+
+        self::assertInstanceOf(CartItem::class, $response);
+        self::assertEquals($value, $this->cart->getQuantity());
+    }
+
+    public function testGetProduct(): void
+    {
+        $value = new Product();
+        $this->cart->setProduct($value);
+        $response = $this->cart;
+
+        self::assertInstanceOf(CartItem::class, $response);
+        self::assertEquals($value, $this->cart->getProduct());
+    }
+
+    public function testGetCustomer(): void
+    {
+        $value = new User();
+        $this->cart->setCustomer($value);
+        $response = $this->cart;
+
+        self::assertInstanceOf(CartItem::class, $response);
+        self::assertEquals($value, $this->cart->getCustomer());
+    }
+}
---
+++ tests/Unit/ProductTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Tests\Unit;
+
+use App\Entity\Product;
+use App\Entity\User;
+use PHPUnit\Framework\TestCase;
+
+class ProductTest extends TestCase
+{
+    private Product $product;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->product = new Product();
+    }
+
+    public function testGetName(): void
+    {
+        $value = 'Product name';
+        $this->product->setName($value);
+        $response = $this->product;
+
+        self::assertInstanceOf(Product::class, $response);
+        self::assertEquals($value, $this->product->getName());
+    }
+
+    public function testGetDescription(): void
+    {
+        $value = 'Product used to generate Shop Websites';
+        $this->product->setDescription($value);
+        $response = $this->product;
+
+        self::assertInstanceOf(Product::class, $response);
+        self::assertEquals($value, $this->product->getDescription());
+    }
+
+    public function testGetQuantity(): void
+    {
+        $value = 15;
+        $this->product->setQuantity($value);
+        $response = $this->product;
+
+        self::assertInstanceOf(Product::class, $response);
+        self::assertEquals($value, $this->product->getQuantity());
+    }
+
+    public function testGetPrice(): void
+    {
+        $value = 55.5;
+        $this->product->setPrice($value);
+        $response = $this->product;
+
+        self::assertInstanceOf(Product::class, $response);
+        self::assertEquals($value, $this->product->getPrice());
+    }
+
+    public function testGetProducer(): void
+    {
+        $value = new User();
+        $this->product->setProducer($value);
+        $response = $this->product;
+
+        self::assertInstanceOf(Product::class, $response);
+        self::assertEquals($value, $this->product->getProducer());
+    }
+}
